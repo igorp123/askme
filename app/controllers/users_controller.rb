6d80f4fb2b6a0192cd63d9884e0a611724059627
Igor <ig.plotnikov@gmail.com>
2018-12-01 18:47:14 +0300

    styles changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Igor',
+        username: 'igor',
+        avatar_url: 'https://pp.userapi.com/c844418/v844418450/92b72/2_U9SbJGuxM.jpg?ava=1'
+        ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'mish'
+      )
+    ]
   end
 
   def new
@@ -19,5 +32,7 @@ class UsersController < ApplicationController
       Question.new(text: 'Как дела?', created_at: Date.parse('28.11.2018')),
       Question.new(text: 'Как жизнь?', created_at: Date.parse('28.11.2018')),
     ]
+
+    @new_question = Question.new()
   end
 end
