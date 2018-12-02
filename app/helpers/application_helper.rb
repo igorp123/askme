bbed9e5b223cd26626712aaeceb3626bfff4e5ea
Igor <ig.plotnikov@gmail.com>
2018-12-02 18:01:49 +0300

    move declension to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def declension(number, vopros, voprosa, voprosov)
+    number = number % 100
+
+    if number.between?(11, 19)
+      voprosov
+    else
+      remainder = number % 10
+
+      case remainder
+      when 1
+        vopros
+      when 2..4
+        voprosa
+      else
+        voprosov
+      end
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,23 +6,4 @@ module UsersHelper
       asset_path 'avatar.jpg'
     end
   end
-
-  def declension(number, vopros, voprosa, voprosov)
-    number = number % 100
-
-    if number.between?(11, 19)
-      voprosov
-    else
-      remainder = number % 10
-
-      case remainder
-      when 1
-        vopros
-      when 2..4
-        voprosa
-      else
-        voprosov
-      end
-    end
-  end
 end
