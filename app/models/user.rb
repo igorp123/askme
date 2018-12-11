fefaee920f9898cca42bda7246e6d725088620c4
Igor <ig.plotnikov@gmail.com>
2018-12-11 15:46:01 +0300

    add dependent to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
 
-  has_many :questions, dependent: :nullify
+  has_many :questions, dependent: :destroy
 
   before_validation :downcase_username
 
