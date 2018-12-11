b7c0333ef3f9595deefd0b231c5b9caba0f3afc3
Igor <ig.plotnikov@gmail.com>
2018-12-11 15:51:17 +0300

    add dependent to users

--- db/migrate/20181211102524_add_author_ref_to_questions.rb
+++ db/migrate/20181211102524_add_author_ref_to_questions.rb
@@ -1,5 +1,5 @@
 class AddAuthorRefToQuestions < ActiveRecord::Migration[5.2]
   def change
-    add_reference :questions, :author, foreign_key: {to_table: :users}
+    add_reference :questions, :author, foreign_key: {to_table: :users, dependent: :nillify}
   end
 end
