337d33e85d756a4e728ac5c23119702f64ac3c05
Igor <ig.plotnikov@gmail.com>
2018-12-11 13:02:32 +0300

    author migration

--- db/migrate/20181211083248_add_author_to_question.rb
+++ db/migrate/20181211083248_add_author_to_question.rb
@@ -1,5 +1,5 @@
 class AddAuthorToQuestion < ActiveRecord::Migration[5.2]
   def change
-    add_reference :questions, :author, foreign_key: true, column: :author_id
+    add_reference :questions, :author, foreign_key: true, column: :user_id
   end
 end
