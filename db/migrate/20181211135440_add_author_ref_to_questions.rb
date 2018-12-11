753f251050682b19f854ddf5eea3d1b290b8bb55
Igor <ig.plotnikov@gmail.com>
2018-12-11 19:42:50 +0300

    new migration

--- db/migrate/20181211135440_add_author_ref_to_questions.rb
+++ db/migrate/20181211135440_add_author_ref_to_questions.rb
@@ -1,5 +1,5 @@
 class AddAuthorRefToQuestions < ActiveRecord::Migration[5.2]
   def change
-    add_reference :questions, :author, foreign_key: { to_table: :users, on_delete: :cascade}
+    add_reference :questions, :author, foreign_key: { to_table: :users, on_delete: ::nullify}
   end
 end
