a9a50c0eeb90705b17e991540383b05b26673266
Igor <ig.plotnikov@gmail.com>
2018-12-11 17:09:48 +0300

    new migration

--- db/migrate/20181211102524_add_author_ref_to_questions.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorRefToQuestions < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :questions, :author, foreign_key: {to_table: :users, dependent: :nillify}
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_102524) do
+ActiveRecord::Schema.define(version: 2018_12_11_135440) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
